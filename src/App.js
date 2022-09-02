fb1ba8a45e4a0a4dd6d1ac463cbb9f530612a5c7
Juan Baz <juan@nayya.com>
2022-09-02 16:50:52 -0300

    add intro

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ const App = () => {
   return (
     <div className="app">
       <h1>Welcome to React Router!</h1>
+      <p>
+        Navigate from an array of routes, and redirects to the next one if the
+        current page should be hidden. If you want to show/hide a page, you can
+        change it on component/SurveyLayout file
+      </p>
+      <p>As default it will bypass Family and Financial pages</p>
       <div className="nav">
         {routes.map((route, index) => (
           <Link key={`route-${index}`} to={route.path}>
